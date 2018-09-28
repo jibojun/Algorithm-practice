3767a020b0907bbe7b764479e460bb73f380a13c
jibojun <jbj19910123@163.com>
2018-09-29 00:04:04 +0800

    update

---
+++ src/main/java/Sort/IntersectionOfTwoArraysII.java
@@ -0,0 +1,34 @@
+package Sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: Bojun Ji
+ * @Description:
+ * @Date: 2018/9/29_12:00 AM
+ */
+public class IntersectionOfTwoArraysII {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0, j = 0; i < nums1.length && j < nums2.length; ) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int[] resultArray = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            resultArray[i] = list.get(i);
+        }
+        return resultArray;
+    }
+}
