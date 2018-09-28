91c25ae187b6161cabbdc9907d4c914c2ca3b6f9
jibj <jibj@Ctrip.com>
2018-09-28 20:05:44 +0800

    update

---
+++ src/main/java/Sort/IntersectionOfTwoArrays.java
@@ -0,0 +1,49 @@
+package Sort;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+/**
+ * @Author: Bojun Ji
+ * @Date: Created in 2018-09-28 19:48
+ * @Description:
+ */
+public class IntersectionOfTwoArrays {
+
+    /**
+     * Given two arrays, write a function to compute their intersection.
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+     * Output: [2]
+     * Example 2:
+     * <p>
+     * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+     * Output: [9,4]
+     * Note:
+     * <p>
+     * Each element in the result must be unique.
+     * The result can be in any order.
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums1.length && i < nums2.length; i++) {
+            if (nums1[i] == nums2[i]) {
+                set.add(nums1[i]);
+            }
+        }
+        int[] resultArray = new int[set.size()];
+        Object[] objectArray = set.toArray();
+        for (int i = 0; i < set.size(); i++) {
+            resultArray[i] = (int) objectArray[i];
+        }
+        return resultArray;
+    }
+}
